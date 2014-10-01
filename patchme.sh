43ed3f9c2f154ba1db5e4c23ee7137c8c9d416f4
Jack Bezalel <ybezalel@gmail.com>
2014-10-01 18:04:30 +0300

    Better notification for unsupported operating systems. HPUX causes warning message but still notifies it is not supported

--- patchme.sh
+++ patchme.sh
@@ -4,13 +4,14 @@
 
 #Functions
 
+FALSE=1
+TRUE=0
+
 . ./get_linux_vendor.sh
 . ./get_os_major_vers.sh
 
 #Main
 
-FALSE=1
-TRUE=0
 VULN="$1"
 HOSTNAME="`hostname`"
 OSNAME="`uname -s`"
@@ -23,32 +24,45 @@ then
 	exit $FALSE
 fi
 
-MACHINE_PATCHED="`find /patches/machines/$HOSTNAME/$VULN/*/patch-live-ok`"
-
-if [ " $MACHINE_PATCHED" != " " ];
-then
-	echo "Machine already marked as patched for $VULN"
-	echo "Exiting...!"
-	exit $TRUE
+if [ -d "/patches/machines/$HOSTNAME/$VULN" ];
+then    
+	MACHINE_PATCHED="`find /patches/machines/$HOSTNAME/$VULN/*/patch-live-ok`"
+	if [ " $MACHINE_PATCHED" != " " ];
+	then
+		echo "Machine already marked as patched for $VULN"
+		echo "Exiting...!"
+		exit $TRUE
+	fi
 fi
 
 
 case $OSNAME in
 Linux)
 	LINUX_VENDOR="`GET_LINUX_VENDOR`"
+	if 	[ $LINUX_VENDOR = "error" ];
+	then
+        	echo "We do not support patching for $OSNAME"
+        	exit $FALSE
+	fi
+	
 	echo "Vendor=$LINUX_VENDOR"
+
 	OS_MAJOR_VERS="`GET_OS_MAJOR_VERS $LINUX_VENDOR`"
+
+	if 	[ $OS_MAJOR_VERS = "error" ];
+	then
+        	echo "We do not support patching for $LINUX_VENDOR"
+        	exit $FALSE
+	fi
+	
 	echo "Major Version=$OS_MAJOR_VERS"
-	MACHINE_PATCH_DIR="/patches/machines/`hostname`/$VULN/$PATCH_DATE"
+	MACHINE_PATCH_DIR="/patches/machines/$HOSTNAME/$VULN/$PATCH_DATE"
 	VULN_PATCH="/patches/vuln/$VULN/$LINUX_VENDOR/$OS_MAJOR_VERS/$OSARCH"
-	mkdir -p $MACHINE_PATCH_DIR
 	echo "Machine $HOSTNAME will be patched at $MACHINE_PATCH_DIR by patch at $VULN_PATCH"
-	echo "Gathering current (pre-patching) YUM and RPM system info"
 
 	if [ $LINUX_VENDOR = "redhat" ];
 	then
 		mkdir -p $MACHINE_PATCH_DIR
-		echo "Machine $HOSTNAME will be patched at $MACHINE_PATCH_DIR by patch at $VULN_PATCH"
 		echo "Gathering current (pre-patching) YUM and RPM system info"
 		rpm -qa &> $MACHINE_PATCH_DIR/software-pre.txt
 		echo "Dry run (verification only) now..."
